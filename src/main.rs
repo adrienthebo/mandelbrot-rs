5d80c095c266d28314ce58c5917b50eb34ad0480
Adrien Thebo <adrien@lagrange-automation.io>
2019-07-27 12:14:10 -0700

    Shenanigans ensued, mandel has been brotted

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-extern crate termion;
-extern crate num;
 extern crate itertools;
+extern crate num;
+extern crate termion;
 
-use std::{thread, time};
+use itertools::Itertools;
 use num::complex::Complex64;
-use std::io::{self, Write, stdout, stdin};
+use std::io::{self, Write};
+use std::{thread, time};
 use termion::raw::IntoRawMode;
 use termion::screen::*;
-use itertools::Itertools;
 
 #[derive(Debug)]
 struct Error {
-    source: Option<Box<std::error::Error>>
+    source: Option<Box<std::error::Error>>,
 }
 
 impl std::fmt::Display for Error {
@@ -28,7 +28,9 @@ impl std::error::Error for Error {
 
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Self {
-        Self { source: Some(Box::new(err)) }
+        Self {
+            source: Some(Box::new(err)),
+        }
     }
 }
 
@@ -44,10 +46,10 @@ impl From<io::Error> for Error {
 ///
 /// If the number does leave the circle before we give up, return `Some(i)`, where
 /// `i` is the number of iterations it took.
-fn escapes(c: num::complex::Complex<f64>, limit: u32) -> Option<u32> {
-    let mut z = num::complex::Complex { re: 0.0, im: 0.0 };
+fn escapes(c: Complex64, limit: u32) -> Option<u32> {
+    let mut z = Complex64 { re: 0.0, im: 0.0 };
     for i in 0..limit {
-        z = z*z + c;
+        z = z * z + c;
         if z.norm_sqr() > 4.0 {
             return Some(i);
         }
@@ -55,7 +57,6 @@ fn escapes(c: num::complex::Complex<f64>, limit: u32) -> Option<u32> {
 
     return None;
 }
-
 
 /// A given X/Y position and Z offset/magnification.
 struct Viewport {
@@ -65,35 +66,54 @@ struct Viewport {
     /// The y axis origin.
     pub y0: f64,
 
-    pub scalar: f64
+    pub scalar: f64,
 }
 
 impl Default for Viewport {
     fn default() -> Self {
-        Self { x0: 0.0, y0: 0.0, scalar: 0.01 }
+        Self {
+            x0: 0.0,
+            y0: 0.0,
+            scalar: 0.1,
+        }
     }
 }
 
-fn paint<W: Write>(w: &mut W, viewport: &Viewport, bounds: (u16, u16)) -> String {
-    //for i in 0..num::clamp(bounds.0, 0, 255u16) {
-
+/// Compute the complex number for a given terminal position, viewport, and bounds.
+fn complex_at(viewport: &Viewport, bounds: (u16, u16), pos: (u16, u16)) -> Complex64 {
     let origin: (i32, i32) = (i32::from(bounds.0 / 2), i32::from(bounds.1 / 2));
+    let offset: (i32, i32) = (i32::from(pos.0) - origin.0, i32::from(pos.1) - origin.1);
 
-    let y_iter = 0 .. bounds.0;
-    let x_iter = 0 .. bounds.1;
+    Complex64 {
+        im: f64::from(offset.0) * viewport.scalar,
+        re: f64::from(offset.1) * viewport.scalar,
+    }
+}
 
-    y_iter.cartesian_product(x_iter)
-        .map(|(yi, xi)| {
-            let c = num::complex::Complex {
-                im: f64::from(i32::from(yi) - origin.0) / viewport.scalar,
-                re: f64::from(i32::from(xi) - origin.1) / viewport.scalar,
-            };
-            let iters = escapes(c, 100).unwrap_or(0) as u8;
+/// Given XY coordinates and computed mandelbrot iteration,
+/// compute the necessary ANSI to move the cursor and paint the cell.
+///
+/// Note: generating strings for every element is highly inefficient; we
+/// should really be appending to a string slice. :shrug:
+fn cell_ansi(pos: (u16, u16), iterations: Option<u32>) -> String {
+    let v = iterations.unwrap_or(0) as u8;
+    format!(
+        "{}{}{}",
+        termion::cursor::Goto(pos.0 + 1, pos.1 + 1),
+        termion::color::Bg(termion::color::Rgb(v, v, v)),
+        iterations.map(|_| ".").unwrap_or(" ")
+    )
+}
 
-            format!("{}{} ",
-                   termion::cursor::Goto(yi + 1, xi + 1),
-                   termion::color::Bg(termion::color::Rgb(iters, iters, iters)))
-        })
+fn frame(viewport: &Viewport, bounds: (u16, u16)) -> String {
+    let y_iter = 0..bounds.0;
+    let x_iter = 0..bounds.1;
+
+    y_iter
+        .cartesian_product(x_iter)
+        .map(|pos| (pos, complex_at(&viewport, bounds, pos)))
+        .map(|(pos, c)| (pos, escapes(c, 100)))
+        .map(|(pos, iter)| cell_ansi(pos, iter))
         .collect()
 }
 
@@ -106,14 +126,17 @@ fn main() -> Result<(), Error> {
     write!(screen, "{}", ToAlternateScreen).unwrap();
     write!(screen, "{}", termion::cursor::Hide).unwrap();
 
-    write!(screen, "{}{}Hello.{}Line 2.",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1),
-           termion::cursor::Goto(1, 3),
-           ).unwrap();
+    write!(
+        screen,
+        "{}{}Hello.{}Line 2.",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Goto(1, 3),
+    )
+    .unwrap();
 
-    let buffer = paint(&mut screen, &viewport, termion::terminal_size().unwrap());
-    write!(screen, "{}", buffer);
+    let buffer = frame(&viewport, termion::terminal_size().unwrap());
+    write!(screen, "{}", buffer).unwrap();
     screen.flush()?;
 
     thread::sleep(time::Duration::from_secs(3));
@@ -122,6 +145,5 @@ fn main() -> Result<(), Error> {
     write!(screen, "{}", termion::cursor::Show).unwrap();
     screen.flush()?;
 
-    println!("dim: {:?}", termion::terminal_size());
     Ok(())
 }
