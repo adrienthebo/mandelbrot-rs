39ad60d904143a70ae376517e7b2f01a482ef48f
Adrien Thebo <adrien@lagrange-automation.io>
2019-11-19 20:22:45 -0300

    main: extract appcmd to frontend

--- src/frontend.rs
+++ src/frontend.rs
@@ -4,6 +4,62 @@
 
 use std::io::Write;
 use termion::raw::IntoRawMode;
+use crate::rctx::RctxTransform;
+use termion::event::Key;
+
+#[derive(Debug, Clone, Copy)]
+pub enum AppCmd {
+    /// Transform the rendering context.
+    Transform(RctxTransform),
+
+    /// Generate a screenshot based on the current rendering context.
+    Save,
+
+    /// Gracefully shut down the app.
+    Quit,
+
+    /// An unhandled command.
+    Unhandled(Key),
+}
+
+impl From<Key> for AppCmd {
+    fn from(key: Key) -> AppCmd {
+        match key {
+            Key::Char('q') => AppCmd::Quit,
+
+            // Zoom in/out - shift key is optional.
+            Key::Char('+') | Key::Char('=') => AppCmd::Transform(RctxTransform::ScaleIn),
+            Key::Char('-') | Key::Char('_') => AppCmd::Transform(RctxTransform::ScaleOut),
+
+            // Move left/right along the real axis.
+            Key::Char('a') => AppCmd::Transform(RctxTransform::TranslateLeft),
+            Key::Char('d') => AppCmd::Transform(RctxTransform::TranslateRight),
+
+            // Move up and down on the imaginary axis.
+            Key::Char('w') => AppCmd::Transform(RctxTransform::TranslateUp),
+            Key::Char('s') => AppCmd::Transform(RctxTransform::TranslateDown),
+
+            // Increase/decrease the limit on iterations to escape.
+            Key::Char('t') => AppCmd::Transform(RctxTransform::IncIterations),
+            Key::Char('g') => AppCmd::Transform(RctxTransform::DecIterations),
+
+            Key::Char('y') => AppCmd::Transform(RctxTransform::IncExp),
+            Key::Char('h') => AppCmd::Transform(RctxTransform::DecExp),
+
+            // Toggle between the Julia sets and the Mandelbrot sets.
+            Key::Char('x') => AppCmd::Transform(RctxTransform::ToggleHolo),
+
+            // Reset the zoom level to default.
+            Key::Char('m') => AppCmd::Transform(RctxTransform::Reset),
+
+            // Generate a state file and image for the current location.
+            Key::Char('p') => AppCmd::Save,
+
+            u => AppCmd::Unhandled(u),
+        }
+    }
+}
+
 
 /// Run a closure in an alternate screen, and disable the alternate screen before handling a
 /// panic.
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ extern crate structopt;
 extern crate termion;
 extern crate tui;
 
-use mandelbrot::frontend;
-use mandelbrot::{loc::Loc, rctx::RctxTransform, rctx::RenderContext, Bounds, Error};
+use mandelbrot::frontend::{self, AppCmd};
+use mandelbrot::{loc::Loc, rctx::RenderContext, Bounds, Error};
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::thread;
@@ -23,59 +23,6 @@ use tui::backend::TermionBackend;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::widgets::{Block, Borders, Widget};
 use tui::Terminal;
-
-#[derive(Debug, Clone, Copy)]
-enum AppCmd {
-    /// Transform the rendering context.
-    Transform(RctxTransform),
-
-    /// Generate a screenshot based on the current rendering context.
-    Save,
-
-    /// Gracefully shut down the app.
-    Quit,
-
-    /// An unhandled command.
-    Unhandled(Key),
-}
-
-impl From<Key> for AppCmd {
-    fn from(key: Key) -> AppCmd {
-        match key {
-            Key::Char('q') => AppCmd::Quit,
-
-            // Zoom in/out - shift key is optional.
-            Key::Char('+') | Key::Char('=') => AppCmd::Transform(RctxTransform::ScaleIn),
-            Key::Char('-') | Key::Char('_') => AppCmd::Transform(RctxTransform::ScaleOut),
-
-            // Move left/right along the real axis.
-            Key::Char('a') => AppCmd::Transform(RctxTransform::TranslateLeft),
-            Key::Char('d') => AppCmd::Transform(RctxTransform::TranslateRight),
-
-            // Move up and down on the imaginary axis.
-            Key::Char('w') => AppCmd::Transform(RctxTransform::TranslateUp),
-            Key::Char('s') => AppCmd::Transform(RctxTransform::TranslateDown),
-
-            // Increase/decrease the limit on iterations to escape.
-            Key::Char('t') => AppCmd::Transform(RctxTransform::IncIterations),
-            Key::Char('g') => AppCmd::Transform(RctxTransform::DecIterations),
-
-            Key::Char('y') => AppCmd::Transform(RctxTransform::IncExp),
-            Key::Char('h') => AppCmd::Transform(RctxTransform::DecExp),
-
-            // Toggle between the Julia sets and the Mandelbrot sets.
-            Key::Char('x') => AppCmd::Transform(RctxTransform::ToggleHolo),
-
-            // Reset the zoom level to default.
-            Key::Char('m') => AppCmd::Transform(RctxTransform::Reset),
-
-            // Generate a state file and image for the current location.
-            Key::Char('p') => AppCmd::Save,
-
-            u => AppCmd::Unhandled(u),
-        }
-    }
-}
 
 /// Convert an RGB image to a series of ANSI escape sequences that set the cursor and paint the
 /// background.
