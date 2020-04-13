3f5a3e9893753cb8ef21901f7e79d59feacbe87c
Adrien Thebo <adrien@lagrange-automation.io>
2020-04-12 19:46:41 -0700

    main: extract frontend-y stuff to appropriate ns

--- src/frontend.rs
+++ src/frontend.rs
@@ -2,10 +2,18 @@
 //!
 //!
 
-use crate::rctx::RctxTransform;
-use std::io::Write;
+use crate::rctx::{Rctx, RctxTransform};
+use crate::Bounds;
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::time::{Instant, SystemTime};
 use termion::event::Key;
+use termion::input::{MouseTerminal, TermRead};
 use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
 
 #[derive(Debug, Clone, Copy)]
 pub enum AppCmd {
@@ -20,6 +28,14 @@ pub enum AppCmd {
 
     /// An unhandled command.
     Unhandled(Key),
+}
+
+/// Configuration for `run` subcommand
+///
+/// TODO: move to a more reasonable place
+#[derive(Debug)]
+pub struct RunOptions {
+    pub img_dir: Option<std::path::PathBuf>,
 }
 
 impl From<Key> for AppCmd {
@@ -87,4 +103,199 @@ fn reset_terminal() -> std::io::Result<()> {
     write!(screen, "{}", termion::screen::ToMainScreen).unwrap();
     write!(screen, "{}", termion::cursor::Show).unwrap();
     screen.flush()
+}
+
+/// Convert an RGB image to a series of ANSI escape sequences that set the cursor and paint the
+/// background.
+///
+/// TODO: make this private
+pub fn img_to_ansi(img: &image::RgbImage, bounds: Bounds) -> String {
+    let mut buf = String::new();
+    for yi in 0..bounds.height {
+        for xi in 0..bounds.width {
+            let pos = crate::Pos { x: xi, y: yi };
+            let pixel = img.get_pixel(xi.into(), yi.into());
+            buf.push_str(String::from(termion::cursor::Goto(pos.x + 1, pos.y + 1)).as_str());
+            buf.push_str(
+                termion::color::Rgb(pixel[0], pixel[1], pixel[2])
+                    .bg_string()
+                    .as_str(),
+            );
+            buf.push(' ');
+        }
+    }
+    buf
+}
+
+pub fn draw_frame<W: Write>(
+    screen: &mut W,
+    rctx: &Rctx,
+    bounds: Bounds,
+) -> Result<(), crate::Error> {
+    let render_start: Instant = Instant::now();
+    let img = rctx.bind(bounds).to_ematrix().to_img(&rctx.colorer);
+    let ansi = img_to_ansi(&img, bounds);
+    let render_stop: Instant = Instant::now();
+
+    let draw_start = Instant::now();
+    write!(screen, "{}", ansi).unwrap();
+    screen.flush()?;
+    let draw_stop = Instant::now();
+
+    let render_delta = render_stop - render_start;
+    let draw_delta = draw_stop - draw_start;
+
+    let labels = vec![
+        format!("fn     = {:?}", &rctx.complexfn),
+        format!("re     = {:.4e}", rctx.loc.re0),
+        format!("im     = {:.4e}", rctx.loc.im0),
+        format!("iter   = {}", rctx.loc.max_iter),
+        format!("scalar = {:.4e}", rctx.loc.scalar),
+        format!("render = {}ms", render_delta.as_millis()),
+        format!("draw   = {}ms", draw_delta.as_millis()),
+    ];
+
+    for (offset, label) in labels.iter().enumerate() {
+        write!(
+            screen,
+            "{}{}{}",
+            termion::cursor::Goto(1, offset as u16 + 1),
+            termion::style::Reset,
+            label
+        )?
+    }
+
+    screen.flush()?;
+    Ok(())
+}
+
+/// Generate an image and location data for a given render context and bounds.
+///
+/// TODO: handle write errors without panicking.
+fn screenshot(rctx: &Rctx, bounds: Bounds) -> Result<(), crate::Error> {
+    let imgen_bounds = Bounds {
+        width: 4000,
+        height: 4000,
+    };
+
+    let imgen_rctx = Rctx {
+        loc: rctx
+            .loc
+            .scale(bounds, imgen_bounds, crate::loc::ScaleMethod::Min),
+        ..rctx.clone()
+    };
+
+    let unix_secs = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .map(|duration| u64::from(duration.as_secs()))
+        .unwrap();
+
+    let json_path = format!("mb-{}.json", unix_secs);
+    File::create(json_path).and_then(|mut f| {
+        let buf = serde_json::to_string(&imgen_rctx).unwrap();
+        f.write_all(&buf.as_bytes())
+    })?;
+
+    let png_path = format!("mb-{}.png", unix_secs);
+    imgen_rctx
+        .bind(imgen_bounds)
+        .to_ematrix()
+        .to_img(&imgen_rctx.colorer)
+        .save(png_path)
+        .map_err(|e| crate::Error::from(e))
+}
+
+/// Accept a key input, act on that input, and indicate if the app should keep going.
+fn handle_key(key: Key, rctx: &mut Rctx, bounds: &Bounds) -> Option<()> {
+    match AppCmd::from(key) {
+        AppCmd::Transform(t) => {
+            rctx.transform(&t);
+            Some(())
+        }
+        AppCmd::Save => {
+            // TODO: handle errors when generating screenshots.
+            let _ = screenshot(&rctx, *bounds);
+            Some(())
+        }
+        AppCmd::Unhandled(_) => Some(()),
+        AppCmd::Quit => None,
+    }
+}
+
+pub fn run_termion(
+    initial_rctx: Rctx,
+    run_options: RunOptions,
+) -> std::result::Result<(), crate::Error> {
+    // Terminal initialization
+    let mut stdin = io::stdin();
+    let stdout = io::stdout().into_raw_mode().unwrap();
+    let mut screen = termion::screen::AlternateScreen::from(stdout);
+    let mut rctx = initial_rctx;
+
+    write!(screen, "{}", termion::screen::ToAlternateScreen).unwrap();
+    write!(screen, "{}", termion::cursor::Hide).unwrap();
+
+    loop {
+        let bounds: Bounds = termion::terminal_size()?.into();
+        draw_frame(&mut screen, &rctx, bounds)?;
+        match (&mut stdin).keys().next() {
+            None | Some(Err(_)) => break, // Stdin was closed or could not be read, shut down.
+            Some(Ok(key)) => {
+                if let None = handle_key(key, &mut rctx, &bounds) {
+                    break;
+                }
+            }
+        }
+    }
+
+    write!(screen, "{}", termion::screen::ToMainScreen).unwrap();
+    write!(screen, "{}", termion::cursor::Show).unwrap();
+    screen.flush()?;
+
+    Ok(())
+}
+
+pub fn run_tui(
+    initial_rctx: Rctx,
+    run_options: RunOptions,
+) -> std::result::Result<(), crate::Error> {
+    // Terminal initialization
+    let mut stdin = std::io::stdin();
+    let stdout = std::io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = termion::screen::AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let mut rctx = initial_rctx;
+    loop {
+        let bounds: Bounds = termion::terminal_size()?.into();
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+                .split(f.size());
+
+            Block::default()
+                .title("Sidebar")
+                .borders(Borders::ALL)
+                .render(&mut f, chunks[0]);
+
+            rctx.render(&mut f, chunks[1]);
+        })?;
+
+        match (&mut stdin).keys().next() {
+            None | Some(Err(_)) => {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+            Some(Ok(key)) => {
+                if let None = handle_key(key, &mut rctx, &bounds) {
+                    break;
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,211 +5,14 @@ extern crate structopt;
 extern crate termion;
 extern crate tui;
 
-use mandelbrot::frontend::{self, AppCmd};
+use indicatif::ProgressBar;
+use mandelbrot::frontend;
 use mandelbrot::rctx::Rctx;
 use mandelbrot::{loc::Loc, Bounds, Error};
 use std::fs::File;
-use std::io::{self, Read, Write};
-use std::thread;
-use std::time::{Duration, Instant, SystemTime};
+use std::io::Read;
+use std::time::Instant;
 use structopt::StructOpt;
-use termion::event::Key;
-use termion::input::{MouseTerminal, TermRead};
-use termion::raw::IntoRawMode;
-use termion::screen::*;
-use indicatif::ProgressBar;
-
-use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout};
-use tui::widgets::{Block, Borders, Widget};
-use tui::Terminal;
-
-/// Convert an RGB image to a series of ANSI escape sequences that set the cursor and paint the
-/// background.
-fn img_to_ansi(img: &image::RgbImage, bounds: Bounds) -> String {
-    let mut buf = String::new();
-    for yi in 0..bounds.height {
-        for xi in 0..bounds.width {
-            let pos = mandelbrot::Pos { x: xi, y: yi };
-            let pixel = img.get_pixel(xi.into(), yi.into());
-            buf.push_str(String::from(termion::cursor::Goto(pos.x + 1, pos.y + 1)).as_str());
-            buf.push_str(
-                termion::color::Rgb(pixel[0], pixel[1], pixel[2])
-                    .bg_string()
-                    .as_str(),
-            );
-            buf.push(' ');
-        }
-    }
-    buf
-}
-
-fn draw_frame<W: Write>(screen: &mut W, rctx: &Rctx, bounds: Bounds) -> Result<(), crate::Error> {
-    let render_start: Instant = Instant::now();
-    let img = rctx.bind(bounds).to_ematrix().to_img(&rctx.colorer);
-    let ansi = img_to_ansi(&img, bounds);
-    let render_stop: Instant = Instant::now();
-
-    let draw_start = Instant::now();
-    write!(screen, "{}", ansi).unwrap();
-    screen.flush()?;
-    let draw_stop = Instant::now();
-
-    let render_delta = render_stop - render_start;
-    let draw_delta = draw_stop - draw_start;
-
-    let labels = vec![
-        format!("fn     = {:?}", &rctx.complexfn),
-        format!("re     = {:.4e}", rctx.loc.re0),
-        format!("im     = {:.4e}", rctx.loc.im0),
-        format!("iter   = {}", rctx.loc.max_iter),
-        format!("scalar = {:.4e}", rctx.loc.scalar),
-        format!("render = {}ms", render_delta.as_millis()),
-        format!("draw   = {}ms", draw_delta.as_millis()),
-    ];
-
-    for (offset, label) in labels.iter().enumerate() {
-        write!(
-            screen,
-            "{}{}{}",
-            termion::cursor::Goto(1, offset as u16 + 1),
-            termion::style::Reset,
-            label
-        )?
-    }
-
-    screen.flush()?;
-    Ok(())
-}
-
-/// Generate an image and location data for a given render context and bounds.
-///
-/// TODO: handle write errors without panicking.
-fn screenshot(rctx: &Rctx, bounds: Bounds) -> Result<(), crate::Error> {
-    let imgen_bounds = Bounds {
-        width: 4000,
-        height: 4000,
-    };
-
-    let imgen_rctx = Rctx {
-        loc: rctx.loc.scale(bounds, imgen_bounds, mandelbrot::loc::ScaleMethod::Min),
-        ..rctx.clone()
-    };
-
-    let unix_secs = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .map(|duration| u64::from(duration.as_secs()))
-        .unwrap();
-
-    let json_path = format!("mb-{}.json", unix_secs);
-    File::create(json_path).and_then(|mut f| {
-        let buf = serde_json::to_string(&imgen_rctx).unwrap();
-        f.write_all(&buf.as_bytes())
-    })?;
-
-    let png_path = format!("mb-{}.png", unix_secs);
-    imgen_rctx
-        .bind(imgen_bounds)
-        .to_ematrix()
-        .to_img(&imgen_rctx.colorer)
-        .save(png_path)
-        .map_err(|e| Error::from(e))
-}
-
-/// Accept a key input, act on that input, and indicate if the app should keep going.
-fn handle_key(key: Key, rctx: &mut Rctx, bounds: &Bounds) -> Option<()> {
-    match AppCmd::from(key) {
-        AppCmd::Transform(t) => {
-            rctx.transform(&t);
-            Some(())
-        }
-        AppCmd::Save => {
-            // TODO: handle errors when generating screenshots.
-            let _ = screenshot(&rctx, *bounds);
-            Some(())
-        }
-        AppCmd::Unhandled(_) => Some(()),
-        AppCmd::Quit => None,
-    }
-}
-
-/// Configuration for `run` subcommand
-#[derive(Debug)]
-struct RunOptions {
-    pub img_dir: Option<std::path::PathBuf>
-}
-
-fn run_termion(initial_rctx: Rctx, run_options: RunOptions) -> std::result::Result<(), crate::Error> {
-    // Terminal initialization
-    let mut stdin = io::stdin();
-    let stdout = io::stdout().into_raw_mode().unwrap();
-    let mut screen = AlternateScreen::from(stdout);
-    let mut rctx = initial_rctx;
-
-    write!(screen, "{}", ToAlternateScreen).unwrap();
-    write!(screen, "{}", termion::cursor::Hide).unwrap();
-
-    loop {
-        let bounds: Bounds = termion::terminal_size()?.into();
-        draw_frame(&mut screen, &rctx, bounds)?;
-        match (&mut stdin).keys().next() {
-            None | Some(Err(_)) => break, // Stdin was closed or could not be read, shut down.
-            Some(Ok(key)) => {
-                if let None = handle_key(key, &mut rctx, &bounds) {
-                    break;
-                }
-            }
-        }
-    }
-
-    write!(screen, "{}", ToMainScreen).unwrap();
-    write!(screen, "{}", termion::cursor::Show).unwrap();
-    screen.flush()?;
-
-    Ok(())
-}
-
-fn run_tui(initial_rctx: Rctx, run_options: RunOptions) -> std::result::Result<(), crate::Error> {
-    // Terminal initialization
-    let mut stdin = io::stdin();
-    let stdout = io::stdout().into_raw_mode()?;
-    let stdout = MouseTerminal::from(stdout);
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
-
-    let mut rctx = initial_rctx;
-    loop {
-        let bounds: Bounds = termion::terminal_size()?.into();
-        terminal.draw(|mut f| {
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
-                .split(f.size());
-
-            Block::default()
-                .title("Sidebar")
-                .borders(Borders::ALL)
-                .render(&mut f, chunks[0]);
-
-            rctx.render(&mut f, chunks[1]);
-        })?;
-
-        match (&mut stdin).keys().next() {
-            None | Some(Err(_)) => {
-                thread::sleep(Duration::from_millis(100));
-            }
-            Some(Ok(key)) => {
-                if let None = handle_key(key, &mut rctx, &bounds) {
-                    break;
-                }
-            }
-        }
-    }
-
-    Ok(())
-}
 
 #[derive(Debug)]
 enum FrontendType {
@@ -308,11 +111,11 @@ fn run(
     rctx.comp = (2.3, 1.0);
 
     let runtime = match frontend_type {
-        None | Some(FrontendType::Termion) => run_termion,
-        Some(FrontendType::Tui) => run_tui,
+        None | Some(FrontendType::Termion) => mandelbrot::frontend::run_termion,
+        Some(FrontendType::Tui) => mandelbrot::frontend::run_tui,
     };
 
-    frontend::run_with_altscreen(move || runtime(rctx, RunOptions { img_dir }))
+    frontend::run_with_altscreen(move || runtime(rctx, frontend::RunOptions { img_dir }))
 }
 
 #[allow(unused)]
